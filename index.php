434cf8e5261cd2fdee780ff467b638d1749cb566
Tina Ostrander <tostrander@greenriver.edu>
2020-04-29 17:32:58 -0400

    Added order functionality

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 //Require the autoload file
 require_once("vendor/autoload.php");
 
@@ -38,10 +41,48 @@ $f3->route('GET /breakfast/green-eggs', function() {
 
 //Reuben route
 $f3->route('GET /lunch/sandwiches/reuben', function() {
-    //echo '<h1>Welcome to my Breakfast Page</h1>';
 
     $view = new Template();
     echo $view->render('views/reuben.html');
+
+});
+
+//Order route
+$f3->route('GET|POST /order', function($f3) {
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+        //["food"]=>"tacos" ["meal"]=>"lunch"
+
+        //Validate the data
+        $meals = array("breakfast", "lunch", "dinner");
+        if (empty($_POST['food']) || !in_array($_POST['meal'], $meals)) {
+            echo "<p>Please enter a food and select a meal</p>";
+        }
+        //Data is valid
+        else {
+            //Store the data in the session array
+            $_SESSION['food'] = $_POST['food'];
+            $_SESSION['meal'] = $_POST['meal'];
+
+            //Redirect to summary page
+            $f3->reroute('summary');
+            session_destroy();
+        }
+    }
+
+    $view = new Template();
+    echo $view->render('views/orderForm.html');
+
+});
+
+//Breakfast route
+$f3->route('GET /summary', function() {
+    //echo '<h1>Thank you for your order!</h1>';
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
 
 });
 
